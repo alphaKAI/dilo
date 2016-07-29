295ce225c77c6e596453445aab8b8913e2130dc1
alphaKAI <alpha.kai.net@alpha-kai-net.info>
2016-07-29 10:51:59 +0900

    reformat - enum KEY_ACTION definition

--- dilo.d
+++ dilo.d
@@ -170,21 +170,21 @@ struct EditorConfig {
 static EditorConfig E;
 
 enum KEY_ACTION {
-  KEY_NULL = 0,       /* NULL */
-  CTRL_C = 3,         /* Ctrl-c */
-  CTRL_D = 4,         /* Ctrl-d */
-  CTRL_F = 6,         /* Ctrl-f */
-  CTRL_H = 8,         /* Ctrl-h */
-  TAB = 9,            /* Tab */
-  CTRL_L = 12,        /* Ctrl+l */
-  ENTER = 13,         /* Enter */
-  CTRL_Q = 17,        /* Ctrl-q */
-  CTRL_S = 19,        /* Ctrl-s */
-  CTRL_U = 21,        /* Ctrl-u */
-  ESC = 27,           /* Escape */
-  BACKSPACE =  127,   /* Backspace */
+  KEY_NULL     = 0,       /* NULL */
+  CTRL_C       = 3,       /* Ctrl-c */
+  CTRL_D       = 4,       /* Ctrl-d */
+  CTRL_F       = 6,       /* Ctrl-f */
+  CTRL_H       = 8,       /* Ctrl-h */
+  TAB          = 9,       /* Tab */
+  CTRL_L       = 12,      /* Ctrl+l */
+  ENTER        = 13,      /* Enter */
+  CTRL_Q       = 17,      /* Ctrl-q */
+  CTRL_S       = 19,      /* Ctrl-s */
+  CTRL_U       = 21,      /* Ctrl-u */
+  ESC          = 27,      /* Escape */
+  BACKSPACE    = 127,    /* Backspace */
   /* The following are just soft codes, not really reported by the terminal directly. */
-  ARROW_LEFT = 1000,
+  ARROW_LEFT   = 1000,
   ARROW_RIGHT,
   ARROW_UP,
   ARROW_DOWN,
